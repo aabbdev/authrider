eb88c211fd4ad0bed0287284bd5b05e67cb64d50
Lucas <lucas.marie@b4finance.com>
2022-12-10 13:17:35 +0100

    fix(wsgi)/bad linting

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,15 @@
-from waitress import serve
-from app import app
+"""WSGI entry point for authrider."""
 import os
 import signal
 import sys
+from waitress import serve
+from app import app
 
 def graceful_exit(_signum, _frame):
-  sys.exit(0)
+    """Gracefully exit the application."""
+    sys.exit(0)
 
 if __name__ == "__main__":
-  signal.signal(signal.SIGTERM, graceful_exit)
-  signal.signal(signal.SIGINT, graceful_exit)
-  serve(app, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
+    signal.signal(signal.SIGTERM, graceful_exit)
+    signal.signal(signal.SIGINT, graceful_exit)
+    serve(app, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
